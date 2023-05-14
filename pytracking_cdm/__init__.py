e4b3afd070a46025054b7cb8bc38dc727e3cb932
Jonas Stettner <mail@jstet.net>
2023-05-15 00:05:51 +0200

    ci: restructure publish pipeline (#6)
    * ci: restructure publish pipeline
    
    * ci: change semantic versioning config

--- pytracking_cdm/__init__.py
+++ pytracking_cdm/__init__.py
@@ -1,6 +1,6 @@
 """Tools for doing sequence analysis on eyetracking fixation data."""
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 from .pytracking_cdm import SeqAnaObj
 
 __all__ = ["SeqAnaObj"]
