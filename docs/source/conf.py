23bdffc9fa7b39929e2ce18b0be7ceb976d48f8b
Jonas Stettner <mail@jstet.net>
2023-05-15 01:57:44 +0200

    docs: change docstring style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,24 +19,28 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.autosummary", 
+"sphinx_autodoc_typehints"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
+autodoc_typehints = "description"  # document types in description
+autoclass_content = "both"  # Add __init__ doc (ie. params) to class summaries
+set_type_checking_flag = True  # Enable 'expensive' imports for sphinx_autodoc_typehints
+# autoapi_dirs = ['../../br_stimpy']
+# autoapi_add_toctree_entry = False
+# autoapi_template_dir = 'autoapi_templates'
 
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = True
-napoleon_use_rtype = True
-napoleon_include_init_with_doc = True
-add_module_names = False
-autodoc_class_signature = "separated"
-highlight_language = "python3"
-autodoc_typehints = "signature"
+# autosummary
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+autosummary_imported_members = True
+add_module_names = False  # Remove namespaces from class/method signatures
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "piccolo_theme"
+html_theme = "piccolo_theme"  
 html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,4 +1,4 @@
-.. pytracking-cdm documentation master file, created by
+.. pytracking_cdm documentation master file, created by
    sphinx-quickstart on Wed May 10 22:11:20 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
